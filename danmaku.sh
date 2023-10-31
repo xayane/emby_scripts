a759f443085a2ccfeb946624668b49c3f04b1b48
xayane <wukun_0312@163.com>
2023-10-31 23:20:57 +0800

    Update danmaku.sh

--- danmaku.sh
+++ danmaku.sh
@@ -5,7 +5,7 @@ content=$(cat index.html)
 
 # 检查index.html是否包含emby-crx
 if grep -q "danmaku.movie.kg" index.html; then
-    echo "Index.html already contains /danmaku.movie.kg, skipping insertion."
+    echo "Index.html already contains danmaku.movie.kg, skipping insertion."
 else
     # 定义要插入的代码
     code='<script type="text/javascript" src="https://danmaku.movie.kg/ext.js"></script>\n<link rel="stylesheet" href="https://danmaku.movie.kg/ext.css"/>\n'
